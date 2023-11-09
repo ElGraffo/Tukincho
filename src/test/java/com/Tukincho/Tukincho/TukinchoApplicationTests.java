72ff58483aea90cf10ff995dfa9ca2e3b59bb2a8
ElGraffoMc <osky_93_07@hotmail.com>
2023-11-08 21:32:20 -0300

    primer commit

---
+++ src/test/java/com/Tukincho/Tukincho/TukinchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tukincho.Tukincho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukinchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
