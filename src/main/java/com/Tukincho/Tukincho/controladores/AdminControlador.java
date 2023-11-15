70baba7b34f556e204263eb5845c12302841ec1e
“anaprada03” <anaprada_5@hotmail.com>
2023-11-14 22:10:51 -0300

    cont Imagen y Admin

---
+++ src/main/java/com/Tukincho/Tukincho/controladores/AdminControlador.java
@@ -0,0 +1,43 @@
+package com.Tukincho.Tukincho.controladores;
+
+
+import com.Tukincho.Tukincho.entidades.Usuario;
+import com.Tukincho.Tukincho.servicios.UsuarioServicio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminControlador {
+    
+    @Autowired
+    private UsuarioServicio usuarioServicio;
+    
+    
+    @GetMapping("/dashboard")
+    public String panelAdministrativo(){
+        return"panel.html";
+    }
+    
+    @GetMapping("/usuarios")
+    public String listar(ModelMap modelo) {
+        List<Usuario>usuarios =usuarioServicio.listarUsuarios();
+        modelo.addAttribute("usuarios", usuarios);
+        
+        return "usuario_list.html";
+    }
+    
+    @GetMapping("/modificarRol/{id}")
+   public String cambiarRol(@PathVariable String id) {
+       usuarioServicio.cambiarRol(id);
+       
+       return "redirect:/admin/usuarios";
+   }
+   
+}
