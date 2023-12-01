c14db021f2e4caf6adf6cd7bdeaebfbda4eb150d
parenteaugusto <augustoparente5@hotmail.com>
2023-11-30 21:37:51 -0300

    probablemente se rompa

--- src/main/java/com/Tukincho/Tukincho/controladores/AdminControlador.java
+++ src/main/java/com/Tukincho/Tukincho/controladores/AdminControlador.java
@@ -1,14 +1,24 @@
 package com.Tukincho.Tukincho.controladores;
 
+<<<<<<< Updated upstream
 import com.Tukincho.Tukincho.entidades.Usuario;
 import com.Tukincho.Tukincho.servicios.UsuarioServicio;
+=======
+>>>>>>> Stashed changes
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.Tukincho.Tukincho.entidades.ServiciosExtra;
+import com.Tukincho.Tukincho.entidades.Usuario;
+import com.Tukincho.Tukincho.repositorios.ServiciosExtraRepositorio;
+import com.Tukincho.Tukincho.servicios.ServiciosExtraServicio;
+import com.Tukincho.Tukincho.servicios.UsuarioServicio;
 
 
 @Controller
@@ -21,7 +31,7 @@ public class AdminControlador {
     
     @GetMapping("/dashboard")
     public String panelAdministrativo(){
-        return"panel.html";
+        return"admin.html";
     }
     
     @GetMapping("/usuarios")
