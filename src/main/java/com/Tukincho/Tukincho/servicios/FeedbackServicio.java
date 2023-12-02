7392c1ceef97461cccabdb8833f6183123357e65
Green-Dragon-Alchemist <127346893+Green-Dragon-Alchemist@users.noreply.github.com>
2023-12-02 18:44:54 -0300

    documentado FeedbackServicio.java

--- src/main/java/com/Tukincho/Tukincho/servicios/FeedbackServicio.java
+++ src/main/java/com/Tukincho/Tukincho/servicios/FeedbackServicio.java
@@ -9,12 +9,25 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+/**
+ * Servicio que gestiona las operaciones relacionadas con los feedbacks en el sistema.
+ *
+ * @Service Indica que esta clase es un servicio de Spring que puede ser inyectado en otras clases.
+ */
 @Service
 public class FeedbackServicio {
     @Autowired
     FeedbackRepositorio feedbackRepositorio;
-
+     /**
+     * Crea un nuevo feedback con la información proporcionada.
+     *
+     * @param titulo Título del feedback.
+     * @param detalle Detalle o contenido del feedback.
+     * @param imagenes Lista de imágenes asociadas al feedback.
+     * @param calificacion Calificación asignada al feedback.
+     * @param usuario Usuario que proporciona el feedback.
+     * @param inmueble Inmueble al que está asociado el feedback.
+     */
     @Transactional
     public void crearFeedback (String titulo, String detalle, List<Imagen> imagenes, Integer calificacion, Usuario usuario, Inmueble inmueble){
         Feedback feedback = new Feedback();
@@ -34,7 +47,15 @@ public class FeedbackServicio {
             e.printStackTrace();
         }
     }
-
+     /**
+     * Edita un feedback existente con la información proporcionada.
+     *
+     * @param id Identificador único del feedback a editar.
+     * @param titulo Nuevo título del feedback.
+     * @param detalle Nuevo detalle o contenido del feedback.
+     * @param imagenes Nueva lista de imágenes asociadas al feedback.
+     * @param calificacion Nueva calificación asignada al feedback.
+     */
     public void editarFeedback(String id, String titulo, String detalle, List<Imagen> imagenes, Integer calificacion){
         Optional<Feedback> feedbackOptional = feedbackRepositorio.findById(id);
         if(feedbackOptional.isPresent()){
@@ -52,7 +73,12 @@ public class FeedbackServicio {
             }
         }
     }
-
+     /**
+     * Busca un feedback por su identificador único.
+     *
+     * @param id Identificador único del feedback a buscar.
+     * @return Feedback encontrado, o null si no se encuentra.
+     */
     public Feedback buscarFeedback(String id){
 
         Optional<Feedback> feedbackOptional = feedbackRepositorio.findById(id);
@@ -61,7 +87,11 @@ public class FeedbackServicio {
         }
         return null;
     }
-
+     /**
+     * Desactiva un feedback existente por su identificador único.
+     *
+     * @param id Identificador único del feedback a desactivar.
+     */
     public void desactivarFeedback(String id){
         Optional <Feedback> feedbackOptional = feedbackRepositorio.findById(id);
         if(feedbackOptional.isPresent()){
