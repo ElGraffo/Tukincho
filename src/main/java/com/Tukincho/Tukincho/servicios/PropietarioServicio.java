644c54d76cf8a41c286a7c695c932d836d59d1b0
Green-Dragon-Alchemist <127346893+Green-Dragon-Alchemist@users.noreply.github.com>
2023-12-02 19:33:20 -0300

    Documentado PropietarioServicio.java

--- src/main/java/com/Tukincho/Tukincho/servicios/PropietarioServicio.java
+++ src/main/java/com/Tukincho/Tukincho/servicios/PropietarioServicio.java
@@ -13,7 +13,11 @@ import com.Tukincho.Tukincho.repositorios.UsuarioRepositorio;
 
 import java.util.List;
 import java.util.Optional;
-
+/**
+ * Servicio que gestiona las operaciones relacionadas con los propietarios en el sistema.
+ *
+ * @Service Indica que esta clase es un servicio de Spring que puede ser inyectado en otras clases.
+ */
 @Service
 public class PropietarioServicio {
     @Autowired
@@ -23,6 +27,13 @@ public class PropietarioServicio {
     @Autowired
     UsuarioRepositorio usuarioRepositorio;
 
+    
+    /**
+     * Crea un nuevo propietario en el sistema a partir de un usuario existente.
+     *
+     * @param usuario Usuario existente que se convertirá en propietario.
+     * @return Propietario creado.
+     */
     public Propietario crearPropietario(Usuario usuario) {
         Propietario propietario = new Propietario();
         propietario.setNombreUsuario(usuario.getNombreUsuario());
@@ -39,7 +50,13 @@ public class PropietarioServicio {
         return null;
     }
 
-
+    /**
+     * Edita la información de un propietario, incluyendo la lista de inmuebles y reservas asociadas.
+     *
+     * @param id Identificador del propietario a editar.
+     * @param inmuebles Lista de inmuebles asociados al propietario.
+     * @param reserva Lista de reservas asociadas al propietario.
+     */
     public void editarPropietario(String id, List<Inmueble> inmuebles, List<Reserva> reserva){
         Optional<Propietario> propietarioOptional = propietarioRepositorio.findById(id);
         if(propietarioOptional.isPresent()){
@@ -54,6 +71,12 @@ public class PropietarioServicio {
         }
     }
 
+    /**
+     * Busca un propietario por su identificador.
+     *
+     * @param id Identificador del propietario a buscar.
+     * @return Propietario encontrado o null si no existe.
+     */
     public Propietario buscarPropietario(String id){
 
         Optional<Propietario> propietarioOptional = propietarioRepositorio.findById(id);
@@ -63,6 +86,11 @@ public class PropietarioServicio {
         return null;
     }
 
+    /**
+     * Obtiene la lista de todos los propietarios registrados en el sistema.
+     *
+     * @return Lista de propietarios o null si ocurre un error.
+     */
     public List<Propietario> listarPropietarios(){
         try{
             return propietarioRepositorio.findAll();
@@ -72,10 +100,22 @@ public class PropietarioServicio {
         }
     }
     
+    /**
+     * Busca un propietario por su nombre de usuario.
+     *
+     * @param nombreUsuario Nombre de usuario del propietario a buscar.
+     * @return Propietario encontrado o null si no existe.
+     */
     public Propietario buscarPropietarioPorNombreUsuario(String nombreUsuario){
         return usuarioRepositorio.buscarUsuarioRolPropietario(nombreUsuario);
     }
-
+    
+    /**
+     * Busca la lista de inmuebles asociados a un propietario por su identificador.
+     *
+     * @param id Identificador del propietario.
+     * @return Lista de inmuebles asociados al propietario o null si ocurre un error.
+     */
     public List<Inmueble> buscarPropietarioPorInmueble(String id){
         try{
             return inmuebleRepositorio.buscarPropietarioPorInmueble(id);
@@ -84,7 +124,12 @@ public class PropietarioServicio {
             return null;
         }
     }
-
+    
+    /**
+     * Desactiva un propietario cambiando su rol a "Usuario" y estableciendo su estado activo a "false".
+     *
+     * @param id Identificador del propietario a desactivar.
+     */
     public void desactivarPropietario(String id){
         Optional <Propietario> propietarioOptional = propietarioRepositorio.findById(id);
         if(propietarioOptional.isPresent()){
