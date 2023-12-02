5ff67cb85cf0bb9e1f350eaa7fce97f9716a8b0f
Green-Dragon-Alchemist <127346893+Green-Dragon-Alchemist@users.noreply.github.com>
2023-12-02 20:39:19 -0300

    Documentado ErroresControlador.java

--- src/main/java/com/Tukincho/Tukincho/controladores/ErroresControlador.java
+++ src/main/java/com/Tukincho/Tukincho/controladores/ErroresControlador.java
@@ -7,10 +7,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
+/**
+ * Controlador que maneja las páginas de error en la aplicación.
+ */
 @Controller
 public class ErroresControlador implements ErrorController {
-
+    
+    /**
+     * Método que renderiza la página de error según el código de error HTTP.
+     *
+     * @param httpRequest Objeto HttpServletRequest que contiene la información de la solicitud.
+     * @return Modelo y vista de la página de error correspondiente.
+     */
     @RequestMapping(value = "/error", method = {RequestMethod.GET, RequestMethod.POST})
     public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
 
@@ -55,10 +63,22 @@ public class ErroresControlador implements ErrorController {
         return errorPage;
 
     }
-
+    
+    /**
+     * Método que obtiene el código de error HTTP de la solicitud.
+     *
+     * @param httpRequest Objeto HttpServletRequest que contiene la información de la solicitud.
+     * @return Código de error HTTP.
+     */
     private int getErrorCode(HttpServletRequest httpRequest) {
         return (Integer) httpRequest.getAttribute("javax.servlet.error.status_code"); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    /**
+     * Método que devuelve la ruta de la página de error.
+     *
+     * @return Ruta de la página de error.
+     */
 
     public String getErrorPath() {
         return "/error.html";
