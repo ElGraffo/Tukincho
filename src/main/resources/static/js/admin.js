168d01d3c22150100d6e225e20fd91c5661885b2
ElGraffo <129801350+ElGraffo@users.noreply.github.com>
2023-12-02 14:02:34 -0300

    Update admin.js
    Modificado admin.js realizando toggle de vistas segun boton

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,61 +1,77 @@
 document.addEventListener("DOMContentLoaded", function() {
     var listaUsuario = document.querySelectorAll('#listaUsuario');
     var listaReserva = document.querySelectorAll('#listaReserva');
-    var propiedad = document.querySelectorAll('#listaInmueble');
+    var listaInmuebles = document.querySelectorAll('#listaInmueble');
 
     listaUsuario.forEach(function(listaUsuario) {
         listaUsuario.addEventListener('click', function() {
             var divListaUsuario = document.querySelectorAll('#divListaUsuario');
+            var divListarReservas = document.querySelectorAll('#divListarReservas');
+            var divListaInmuebles = document.querySelectorAll('#divListaInmuebles');
 
             divListaUsuario.forEach(function(divListaUsuario) {
                 var clase = divListaUsuario.getAttribute("class");
 
-                if(clase.includes("novisible")) {
-                    divListaUsuario.removeAttribute("class");
-                    divListaUsuario.setAttribute("class","visible");
-                } else {
-                    divListaUsuario.removeAttribute("class");
-                    divListaUsuario.setAttribute("class","novisible");
-                }
+                divListaUsuario.setAttribute("class","visible");
+            });
+            divListarReservas.forEach(function(divListarReservas) {
+                var clase = divListarReservas.getAttribute("class");
+
+                divListarReservas.setAttribute("class","novisible");
+            });
+            divListaInmuebles.forEach(function(divListaInmuebles) {
+                var clase = divListaInmuebles.getAttribute("class");
+
+                divListaInmuebles.setAttribute("class","novisible");
             });
         });
     });
 
     listaReserva.forEach(function(listaReserva) {
         listaReserva.addEventListener('click', function() {
+            var divListaUsuario = document.querySelectorAll('#divListaUsuario');
             var divListarReservas = document.querySelectorAll('#divListarReservas');
+            var divListaInmuebles = document.querySelectorAll('#divListaInmuebles');
 
+            divListaUsuario.forEach(function(divListaUsuario) {
+                var clase = divListaUsuario.getAttribute("class");
+
+                divListaUsuario.setAttribute("class","novisible");
+            });
             divListarReservas.forEach(function(divListarReservas) {
                 var clase = divListarReservas.getAttribute("class");
 
-                if(clase.includes("novisible")) {
-                    divListarReservas.removeAttribute("class");
-                    divListarReservas.setAttribute("class","visible");
-                } else {
-                    divListarReservas.removeAttribute("class");
-                    divListarReservas.setAttribute("class","novisible");
-                }
+                divListarReservas.setAttribute("class","visible");
             });
-
-        });
-    });
-
-    listaInmueble.forEach(function(listaInmueble) {
-        listaInmueble.addEventListener('click', function() {
-            var divListaInmuebles = document.querySelectorAll('#divListaInmuebles');
-
             divListaInmuebles.forEach(function(divListaInmuebles) {
                 var clase = divListaInmuebles.getAttribute("class");
 
-                if(clase.includes("novisible")) {
-                    divListaInmuebles.removeAttribute("class");
-                    divListaInmuebles.setAttribute("class","visible");
-                } else {
-                    divListaInmuebles.removeAttribute("class");
-                    divListaInmuebles.setAttribute("class","novisible");
-                }
+                divListaInmuebles.setAttribute("class","novisible");
             });
+        });
+    });
 
+    listaInmuebles.forEach(function(listaUsuario) {
+        listaInmuebles.addEventListener('click', function() {
+            var divListaUsuario = document.querySelectorAll('#divListaUsuario');
+            var divListarReservas = document.querySelectorAll('#divListarReservas');
+            var divListaInmuebles = document.querySelectorAll('#divListaInmuebles');
+
+            divListaUsuario.forEach(function(divListaUsuario) {
+                var clase = divListaUsuario.getAttribute("class");
+
+                divListaUsuario.setAttribute("class","novisible");
+            });
+            divListarReservas.forEach(function(divListarReservas) {
+                var clase = divListarReservas.getAttribute("class");
+
+                divListarReservas.setAttribute("class","novisible");
+            });
+            divListaInmuebles.forEach(function(divListaInmuebles) {
+                var clase = divListaInmuebles.getAttribute("class");
+
+                divListaInmuebles.setAttribute("class","visible");
+            });
         });
     });
 });
