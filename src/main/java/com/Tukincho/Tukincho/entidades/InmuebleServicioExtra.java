a343b59d08a3fdd1cbab856f822581a11578f4d9
Green-Dragon-Alchemist <127346893+Green-Dragon-Alchemist@users.noreply.github.com>
2023-12-02 16:38:29 -0300

    documentado InmuebleServicioExtra.java

--- src/main/java/com/Tukincho/Tukincho/entidades/InmuebleServicioExtra.java
+++ src/main/java/com/Tukincho/Tukincho/entidades/InmuebleServicioExtra.java
@@ -8,6 +8,14 @@ import javax.persistence.ManyToOne;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
+/**
+ * Clase que representa la relación entre un inmueble y un servicio extra en el sistema.
+ * Contiene información sobre la asociación de un inmueble con un servicio extra y el precio asociado.
+ *
+ * @Entity Indica que esta clase es una entidad que se puede almacenar en una base de datos.
+ * @Data Anotación de Lombok que genera automáticamente métodos toString, equals, hashCode, y otros métodos útiles.
+ */
+
 @Entity
 @Data
 public class InmuebleServicioExtra {
