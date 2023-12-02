35f344bbf243cb426900596a5a9e5bb6e235a95e
Green-Dragon-Alchemist <127346893+Green-Dragon-Alchemist@users.noreply.github.com>
2023-12-02 19:25:56 -0300

    documentado InmuebleServicio.java

--- src/main/java/com/Tukincho/Tukincho/servicios/InmuebleServicio.java
+++ src/main/java/com/Tukincho/Tukincho/servicios/InmuebleServicio.java
@@ -17,6 +17,12 @@ import org.springframework.web.multipart.MultipartFile;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Servicio que gestiona las operaciones relacionadas con los inmuebles en el sistema.
+ *
+ * @Service Indica que esta clase es un servicio de Spring que puede ser inyectado en otras clases.
+ */
+
 @Service
 public class InmuebleServicio {
 
@@ -26,7 +32,23 @@ public class InmuebleServicio {
     ImagenServicio imagenServicio;
     @Autowired
     ServiciosExtraRepositorio serviciosExtraRepositorio;
-    
+
+    /**
+     * Crea un nuevo inmueble con la información proporcionada, incluyendo imágenes y servicios extras.
+     *
+     * @param propietario Propietario del inmueble.
+     * @param inmueble Objeto que representa el inmueble a crear.
+     * @param nombre Nombre del inmueble.
+     * @param descripcionDelInmueble Descripción del inmueble.
+     * @param precioPorNoche Precio por noche del inmueble.
+     * @param otrosDetallesDelInmueble Otros detalles del inmueble.
+     * @param direccion Dirección del inmueble.
+     * @param provincia Provincia del inmueble.
+     * @param reserva Lista de reservas asociadas al inmueble.
+     * @param imagenes Lista de archivos de imágenes asociadas al inmueble.
+     * @param serviciosExtras Lista de servicios extras asociados al inmueble.
+     * @throws Exception Si ocurre un error durante la operación.
+     */
     @Transactional
     public void crearInmueble(Propietario propietario,Inmueble inmueble,
             String nombre, 
@@ -67,6 +89,23 @@ public class InmuebleServicio {
         inmuebleRepositorio.save(inmueble);
     }
 
+    
+    /**
+     * Edita un inmueble existente con la información proporcionada, incluyendo imágenes y servicios extras.
+     *
+     * @param id Identificador único del inmueble a editar.
+     * @param nombre Nuevo nombre del inmueble.
+     * @param descripcionDelInmueble Nueva descripción del inmueble.
+     * @param precioPorNoche Nuevo precio por noche del inmueble.
+     * @param otrosDetallesDelInmueble Nuevos detalles del inmueble.
+     * @param direccion Nueva dirección del inmueble.
+     * @param provincia Nueva provincia del inmueble.
+     * @param reserva Nueva lista de reservas asociadas al inmueble.
+     * @param serviciosExtras Nueva lista de servicios extras asociados al inmueble.
+     * @param activa Nuevo estado de activación del inmueble.
+     * @param imagenes Nueva lista de archivos de imágenes asociadas al inmueble.
+     * @throws Exception Si ocurre un error durante la operación.
+     */
     @Transactional
     public void editarInmueble(String id, String nombre,String descripcionDelInmueble, Long precioPorNoche,
                                String otrosDetallesDelInmueble, String direccion, Provincia provincia, List<Reserva> reserva,
@@ -150,7 +189,17 @@ public class InmuebleServicio {
             e.printStackTrace();
         }
     }
-
+    /**
+     * Realiza la validación de los campos del inmueble.
+     *
+     * @param nombre Nombre del inmueble.
+     * @param descripcionDelInmueble Descripción del inmueble.
+     * @param precioPorNoche Precio por noche del inmueble.
+     * @param otrosDetallesDelInmueble Otros detalles del inmueble.
+     * @param direccion Dirección del inmueble.
+     * @param provincia Provincia del inmueble.
+     * @throws Exception Si algún campo no cumple con los requisitos.
+     */
     private void validar(String nombre, String descripcionDelInmueble, Long precioPorNoche,
             String otrosDetallesDelInmueble, String direccion, Provincia provincia) throws Exception {
 
