fe52e31cd2982e349b06f774502d158d9385cb71
Green-Dragon-Alchemist <127346893+Green-Dragon-Alchemist@users.noreply.github.com>
2023-12-02 19:28:19 -0300

    documentado InmuebleServicioExtraServicio.java

--- src/main/java/com/Tukincho/Tukincho/servicios/InmuebleServicioExtraServicio.java
+++ src/main/java/com/Tukincho/Tukincho/servicios/InmuebleServicioExtraServicio.java
@@ -12,15 +12,32 @@ import org.springframework.stereotype.Service;
  * @version 1.0
  * @data 18/11/2023
  */
+
+/**
+ * Servicio que gestiona las operaciones relacionadas con los servicios extras asociados a los inmuebles en el sistema.
+ *
+ * @Service Indica que esta clase es un servicio de Spring que puede ser inyectado en otras clases.
+ */
+
 @Service
 public class InmuebleServicioExtraServicio {
     @Autowired
     private InmuebleServicioExtraRepositorio inmuebleServicioExtraRepositorio;
-
+    
+    /**
+     * Guarda un nuevo servicio extra asociado a un inmueble en la base de datos.
+     *
+     * @param inmuebleServicioExtra Objeto que representa el servicio extra asociado a un inmueble.
+     */
     public void guardarInmuebleServicioExtra(InmuebleServicioExtra inmuebleServicioExtra) {
         inmuebleServicioExtraRepositorio.save(inmuebleServicioExtra);
     }
 
+    /**
+     * Obtiene todos los servicios extras asociados a los inmuebles almacenados en la base de datos.
+     *
+     * @return Lista de todos los servicios extras asociados a los inmuebles.
+     */
     public List<InmuebleServicioExtra> obtenerTodosLosInmuebleServicioExtra() {
         return inmuebleServicioExtraRepositorio.findAll();
     }
