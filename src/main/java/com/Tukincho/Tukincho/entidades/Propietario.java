f654448806304b4e96fdacb0bb09b8be9466022a
Green-Dragon-Alchemist <127346893+Green-Dragon-Alchemist@users.noreply.github.com>
2023-12-02 16:36:12 -0300

    documentado Propietario.java

--- src/main/java/com/Tukincho/Tukincho/entidades/Propietario.java
+++ src/main/java/com/Tukincho/Tukincho/entidades/Propietario.java
@@ -3,7 +3,14 @@ package com.Tukincho.Tukincho.entidades;
 import lombok.Data;
 import javax.persistence.*;
 import java.util.List;
-
+/**
+ * Clase que representa un propietario en el sistema.
+ * Hereda de la clase Usuario y contiene información adicional sobre los inmuebles y reservas asociadas.
+ *
+ * @Entity Indica que esta clase es una entidad que se puede almacenar en una base de datos.
+ * @Data Anotación de Lombok que genera automáticamente métodos toString, equals, hashCode, y otros métodos útiles.
+ * @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) Especifica la estrategia de herencia, utilizando una tabla por clase.
+ */
 @Entity
 @Data
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
