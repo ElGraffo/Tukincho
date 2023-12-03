43ce89fe351a0f8cca795413ba8baaee213f68ac
Green-Dragon-Alchemist <127346893+Green-Dragon-Alchemist@users.noreply.github.com>
2023-12-03 00:11:07 -0300

    Documentado ImagenRepositorio.java

--- src/main/java/com/Tukincho/Tukincho/repositorios/ImagenRepositorio.java
+++ src/main/java/com/Tukincho/Tukincho/repositorios/ImagenRepositorio.java
@@ -5,7 +5,10 @@ package com.Tukincho.Tukincho.repositorios;
 import com.Tukincho.Tukincho.entidades.Imagen;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+ * Repositorio que gestiona las operaciones de persistencia para la entidad Imagen.
+ * Utiliza Spring Data JPA y extiende de JpaRepository para proporcionar métodos de acceso a datos comunes.
+ */
 @Repository
 public interface ImagenRepositorio extends JpaRepository<Imagen, String>{
 }
