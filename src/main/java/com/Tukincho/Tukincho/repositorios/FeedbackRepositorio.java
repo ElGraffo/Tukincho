ed97452a3079675d3e357a0c6292a809a7ccbbb3
Green-Dragon-Alchemist <127346893+Green-Dragon-Alchemist@users.noreply.github.com>
2023-12-03 00:08:47 -0300

    Update FeedbackRepositorio.java

--- src/main/java/com/Tukincho/Tukincho/repositorios/FeedbackRepositorio.java
+++ src/main/java/com/Tukincho/Tukincho/repositorios/FeedbackRepositorio.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+/**
+ * Repositorio que gestiona las operaciones de persistencia para la entidad Feedback.
+ * Utiliza Spring Data JPA y extiende de JpaRepository para proporcionar métodos de acceso a datos comunes.
+ */
 @Repository
 public interface FeedbackRepositorio extends JpaRepository<Feedback, String> {
 
