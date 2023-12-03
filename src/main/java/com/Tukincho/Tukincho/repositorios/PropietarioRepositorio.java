fb2111abe99df0f2b5e7e20f462df306c6ec4c2c
Green-Dragon-Alchemist <127346893+Green-Dragon-Alchemist@users.noreply.github.com>
2023-12-03 00:17:30 -0300

    Documentado PropietarioRepositorio.java

--- src/main/java/com/Tukincho/Tukincho/repositorios/PropietarioRepositorio.java
+++ src/main/java/com/Tukincho/Tukincho/repositorios/PropietarioRepositorio.java
@@ -8,8 +8,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repositorio que gestiona las operaciones de persistencia para la entidad Propietario.
+ * Utiliza Spring Data JPA y extiende de JpaRepository para proporcionar métodos de acceso a datos comunes.
+ */
 @Repository
 public interface PropietarioRepositorio extends JpaRepository<Propietario, String> {
+      /**
+     * Busca un propietario por su nombre de usuario.
+     *
+     * @param nombreUsuario El nombre de usuario del propietario a buscar.
+     * @return Lista de propietarios que coinciden con el nombre de usuario proporcionado.
+     */
       @Query("select p from Propietario p where p.nombreUsuario = :nombreUsuario")
     List< Propietario> buscarPorPropietario(@Param("nombreUsuario") String nombreUsuario);
 
